f0e4c33b3ed30518f84699c8b32d61fd4c7411bf
Kevin Langley <me@kevinlangleyjr.com>
2014-11-23 14:35:14 -0500

    adding changes to footer and more descriptive readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #WP Skeleton#
+
+This is a starter theme based off of [_s](http://underscores.me) by [Automattic](http://automattic.com) with [Zurb's](http://zurb.com/) [Foundation](https://github.com/zurb/foundation) integrated into the styles.
+
+This is mainly for my own personal use, but if others find it useful and would like to contribute just submit an issue or pull request. Thanks!
--- wp-content/themes/wp-skeleton/footer.php
+++ wp-content/themes/wp-skeleton/footer.php
@@ -13,9 +13,11 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
 			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp_skeleton' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wp_skeleton' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp_skeleton' ) ); ?>" target="_blank"><?php printf( __( 'Proudly powered by %s', 'wp_skeleton' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( __( 'Theme: %1$s by %2$s.', 'wp_skeleton' ), 'wp_skeleton', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+				<?php printf( __( 'Theme: %1$s by %2$s', 'wp_skeleton' ), 'wp_skeleton', '<a href="http://kevinlangleyjr.com/" target="_blank" rel="designer">Kevin Langley Jr.</a>' ); ?>
+				<span class="sep"> | </span>
+				Based off of <a href="http://underscores.me/" target="_blank">_s</a> by <a href="http://automattic.com/" target="_blank">Automattic</a>
 			</div><!-- .site-info -->
 		</div><!-- .container -->
 	</footer><!-- #colophon -->
